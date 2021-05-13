e3f9eefd383700777c99fc576c948742efb3f488
Alex <axherrerax@gmail.com>
2021-05-13 10:30:36 -0400

    fetch

--- src/store/flux.js
+++ src/store/flux.js
@@ -18,12 +18,14 @@ const getState = ({ getStore, getActions, setStore }) => {
 			// Use getActions to call a function within a fuction
 			/* PROFILES */
 			getProfile: () => {
-				fetch("http://localhost:5000/user/profile")
+				fetch("http://localhost:5000/user/profile/1")
 				.then( (resp) => resp.json())
 				.then((data) => {
+					console.log(data)
 					setStore({
 						profile: data,
 					});
+					
 				});
 			},
 
